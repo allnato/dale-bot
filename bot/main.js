d1c50681630c6aa0ef3b5855421197fa8f339bb6
Allendale Nato <natoallendale@gmail.com>
2017-06-07 01:45:52 +0800

    Add authentication

--- bot/main.js
+++ bot/main.js
@@ -1,1 +1,14 @@
-require('dotenv').config();
+const client = require('./twitter-connect');
+
+// Verify Authentication
+client.get('account/verify_credentials',{})
+  .then(() => {
+    console.log('Successfully Authenticated.');
+  }, err => {
+    console.log('Authentication failed: Invalid credentials.');
+    console.log(err);
+    process.exit(1);
+  });
+
+
+  
