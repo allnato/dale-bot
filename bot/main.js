0106cea5f0562f84277054ae5976936b65261074
Allendale Nato <natoallendale@gmail.com>
2017-06-24 21:17:33 +0800

    Modify API verification to include reddit-api

--- bot/main.js
+++ bot/main.js
@@ -2,35 +2,48 @@
  * Syllable Counter bot.
  */
 const twitter = require('./api/twitter-api');
+const reddit = require('./api/reddit-api');
 const logger = require('./utils/logger');
 const syllableRequest = require('./utils/twitter/syllableRequest');
 
-// Twitter user stream connection
-const stream = twitter.getUserStream();
+/**
+ * Set all API variables to null.
+ */
+let twitterStream = null;
+let twitterUser = null;
+let redditUser = null;
 
-
-let user = null;
-// Verify Authentication
-twitter.verifyCredentials()
-    .then((res) => {
-        user = res;
-        logger.info(`Successfully Authenticated as "${user.screen_name}"`);
-        main();
-    }, err => {
+/**
+ * Verify Credentials for both twitter and reddit.
+ * If both authentication are verified, invoke the main() function.
+ */
+console.log('AUTHENTICATING APIs...');
+Promise.all([twitter.verifyCredentials(), reddit.verifyCredentials()])
+    .then(res => {
+        twitterUser = res[0];
+        redditUser = res[1];
+        logger.info(`Successfully authenticated in twitter as "${twitterUser.screen_name}"`);
+        logger.info(`Successfully authenticated in reddit as "${redditUser.name}"`);
+        // Twitter stream conenction
+        twitterStream = twitter.getUserStream();
+        main();        
+    }).catch( err => {
         logger.error('Authentication failed: Invalid credentials.', {error: err});
-        process.exit(1);
+        logger.error(err);
     });
+
 
 // Main function
 function main() {
+    logger.info('STARTING main function.');
     /** 
      * Listen reply tweets to return a syllable request.
      */
-    stream.on('data', res => {
+    twitterStream.on('data', res => {
         let reply_to_name = res.in_reply_to_screen_name;
         let reply_to_status = res.in_reply_to_status_id;
 
-        if (reply_to_name === user.screen_name && reply_to_status == null) {
+        if (reply_to_name === twitterUser.screen_name && reply_to_status == null) {
             let reply_from = res.user.screen_name;
             syllableRequest(res.id_str, reply_from, res.text)
                 .then((res) => {
