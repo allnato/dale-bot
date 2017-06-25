84f312aa98933bed38625143db39f4b1d21b595e
Allendale Nato <natoallendale@gmail.com>
2017-06-25 15:54:10 +0800

    Remove phonemes in syllableRequest

--- bot/utils/twitter/syllableRequest.js
+++ bot/utils/twitter/syllableRequest.js
@@ -5,7 +5,7 @@ const countSyllables = require('./../lang/countSyllables');
 const syllableRequest = (status_id, screen_name, text) => {
     text = filterMentions(text);
     let syllables = countSyllables(text);
-    let message = `@${screen_name}\nSyllable Count: ${syllables.count}\nPhonemes: ${syllables.phonemes}`;
+    let message = `@${screen_name}\nSyllable Count: ${syllables.count}`;
 
     let params = {
         in_reply_to_status_id: status_id, 
