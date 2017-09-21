a73b1bcb7167f0f35d355524375f3a3476d93d9c
Allendale Nato <natoallendale@gmail.com>
2017-09-21 19:51:47 +0800

    Add Dark sky API

--- bot/api/api-test.js
+++ bot/api/api-test.js
@@ -1,8 +1,16 @@
 const {getLatLong} = require('./gmapsGeocoding-api.js');
+const {getForecast} = require('./darkSky-api');
 
-getLatLong('Manila, Philippines')
-    .then(res => {
-        console.log(res);
-    }).catch(err => {
+
+const getWeather = async(address) => {
+    try {
+        let geocode = await getLatLong(address);
+        let forecast = await getForecast(geocode.latitude, geocode.longitude);
+
+        console.log(forecast);
+    } catch (err) {
         console.log(err);
-    });
+    }
+}
+
+getWeather('Taft Avenue, Manila, Philippines');
--- bot/api/darkSky-api.js
+++ bot/api/darkSky-api.js
@@ -3,7 +3,18 @@ const key = require('./../config/config').darkskyKey;
 const apiBaseURL = 'https://api.darksky.net/forecast/' + key;
 
 const getForecast = async(lat, long) => {
+    let url = `${apiBaseURL}/${lat},${long}?units=si`;
+    let jsonRes = await axios.get(url, {responseType: 'json'});
 
+    if (jsonRes.status == 400) {
+        throw new Error(jsonRes.data.error);
+    } else if (jsonRes.status == 403) {
+        throw new Error('Invalid API Key');
+    } else if (jsonRes.status == 200) {
+        return jsonRes.data.currently;
+    } else {
+        throw new Error('Error connecting to Dark Sky API');
+    }
 };
 
 module.exports = {
