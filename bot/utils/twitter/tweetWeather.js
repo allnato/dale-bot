76326fa424405c3930855652d69ee603ab7df5e7
Allendale Nato <natoallendale@gmail.com>
2017-09-24 18:57:35 +0800

    Update weather tweet message to only tweetHead

--- bot/utils/twitter/tweetWeather.js
+++ bot/utils/twitter/tweetWeather.js
@@ -21,7 +21,9 @@ const tweetWeather = async(address='Manila, Philippines') => {
         
         let tweetHead = `As of ${curTime} in ${address}\nThere will be ${curForecast.hourly.summary}\n\n`;
         let tweetBody = `Weather Status: ${data.status}\nTemperature: ${data.temp}\nPrecipitation: ${data.precip}\nHumidity: ${data.humid}`;
-        await postStatus({status: tweetHead + tweetBody});
+        // TODO: Find a workaround to post a tweet status over 140 characters.
+        let tweetPost = await postStatus({status: tweetHead});
+        logger.info('Tweeted a new weather update', {tweet: tweetPost.text});
 
     } catch (err) {
         logger.error('Error tweeting current weather forecast', {error: err});
