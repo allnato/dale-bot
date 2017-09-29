8596d79f982a149b01b37bf7096125e75aa4a39b
Allendale Nato <natoallendale@gmail.com>
2017-09-29 16:54:20 +0800

    Update docsting doumentation in getWeather twitter module

--- bot/utils/weather/getWeather.js
+++ bot/utils/weather/getWeather.js
@@ -1,7 +1,16 @@
+/**
+ * @module
+ * Fetches the weather data of the given address via DarkSky API.
+ */
 const logger = require('./../logger');
 const getGeocode = require('./../../api/gmapsGeocoding-api');
 const getForecast = require('./../../api/darkSky-api');
 
+/**
+ * Returns a Promise containing the longitude and latitude of the address.
+ * @param {String} address Adress input
+ * @returns {Promise<Object>} Returns a formatted address, longitude, and latitude
+ */
 const getLatLong = async(address) => {
     let geocode = await getGeocode(address);
     // Take the first value
@@ -14,6 +23,11 @@ const getLatLong = async(address) => {
     };
 }
 
+/**
+ * Fetches the current weather forecast of the given address.
+ * @param {*} address Address input
+ * @returns {Promise<Object>} Returns the current hour and daily forecast.
+ */
 const getCurrForecast = async(address) => {
     let coord = await getLatLong(address);
     let weather = await getForecast(coord.latitude, coord.longitude);
